569a8e8d32f71ff453f8eab5ed71e112140f34c8
Corey Gale <corey@gumgum.com>
2019-12-04 00:12:01 -0800

    Enable credstash secrets & increase function timeout to 3 minutes

--- terraform/modules/lambda-analyzer/lambda.tf
+++ terraform/modules/lambda-analyzer/lambda.tf
@@ -16,15 +16,13 @@ resource "aws_lambda_function" "lambda_function" {
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
   runtime          = "python3.6"
   memory_size      = "512"
-  timeout          = "30"
+  timeout          = "180"
 
-  kms_key_arn = "arn:aws:kms:us-east-1:778129680386:key/7fb9ced2-39df-4085-98ee-83b08ac3bc2b"
   environment {
     variables = {
-      TEST = "hi"
-      # GOOGLE_SERVICE_CREDS_JSON = data.credstash_secret.google_creds_json.value
-      # JIRA_API_EMAIL            = data.credstash_secret.jira_api_email.value
-      # JIRA_API_TOKEN            = data.credstash_secret.jira_api_token.value
+      GOOGLE_SERVICE_CREDS_JSON = data.credstash_secret.google_creds_json.value
+      JIRA_API_EMAIL            = data.credstash_secret.jira_api_email.value
+      JIRA_API_TOKEN            = data.credstash_secret.jira_api_token.value
     }
   }
 
