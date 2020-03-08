c008ebcc146e6f6920a62d151e4d326f7d0f22ff
Corey Gale <corey@gumgum.com>
2020-03-07 17:47:02 -0800

    Test volume mounts

--- terragrunt-action/entrypoint.sh
+++ terragrunt-action/entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh -l
 
-echo $GITHUB_WORKSPACE
+ls -laht $GITHUB_WORKSPACE
 
-ls -laht /github/
-ls -laht /github/home/
-ls -laht /github/workflow/
-ls -laht /github/workspace/
+pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t exporter
 
-pip install -r /github/workspace/exporter/requirements.txt -t exporter
-
-export TF_VAR_repo_root_path=/github/workspace/ && \
-	cp /github/workspace/config/$1.yml /github/workspace/exporter/config.yml && \
-	cd /github/workspace/terraform/terragrunt/$1 && \
+export TF_VAR_repo_root_path=$GITHUB_WORKSPACE/ && \
+	cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml && \
+	cd $GITHUB_WORKSPACE/terraform/terragrunt/$1 && \
 	terragrunt $2 --terragrunt-source-update --auto-approve=true
