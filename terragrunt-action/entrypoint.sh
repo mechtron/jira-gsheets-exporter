30f79da81294c1bf7ac033bf41c2c929981da947
Corey Gale <corey@gumgum.com>
2020-03-07 23:05:59 -0800

    Re-factor

--- terragrunt-action/entrypoint.sh
+++ terragrunt-action/entrypoint.sh
@@ -1,11 +1,17 @@
 #!/bin/sh -l
 
-pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t $GITHUB_WORKSPACE/exporter/
+# Install app dependencies
+pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t $GITHUB_WORKSPACE/exporter
 
+# Copy app config.yml
+cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml
+
+# Set Terraform vars
 export TF_VAR_repo_root_path=$GITHUB_WORKSPACE
 export TF_VAR_google_service_creds_json=$GOOGLE_SERVICE_CREDS_JSON
 export TF_VAR_jira_api_email=$JIRA_API_EMAIL
 export TF_VAR_jira_api_token=$JIRA_API_TOKEN
-cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml
+
+# Run Terragrunt
 cd $GITHUB_WORKSPACE/terraform/terragrunt/$1
 terragrunt $2 --terragrunt-source-update --auto-approve=true
