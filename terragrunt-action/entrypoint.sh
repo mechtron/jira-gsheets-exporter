dfe8645160830b4ddc2dec218c5282e33780f428
Corey Gale <corey@gumgum.com>
2020-03-07 19:02:29 -0800

    Testing

--- terragrunt-action/entrypoint.sh
+++ terragrunt-action/entrypoint.sh
@@ -5,6 +5,8 @@ pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t exporter
 aws sts get-caller-identity
 aws s3 ls
 
+echo $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+
 export TF_VAR_repo_root_path=$GITHUB_WORKSPACE/ && \
 	cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml && \
 	cd $GITHUB_WORKSPACE/terraform/terragrunt/$1 && \
