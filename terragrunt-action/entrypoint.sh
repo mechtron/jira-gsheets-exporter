042b0e49f51d51889ae64a9b492c0cb27359c223
Corey Gale <corey@gumgum.com>
2020-03-07 21:16:59 -0800

    Re-factor

--- terragrunt-action/entrypoint.sh
+++ terragrunt-action/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/sh -l
 
-pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t $GITHUB_WORKSPACE/exporter
+pip install -r $GITHUB_WORKSPACE/exporter/requirements.txt -t $GITHUB_WORKSPACE/exporter/
 
-aws sts get-caller-identity
-
-export TF_VAR_repo_root_path=$GITHUB_WORKSPACE && \
-export TF_VAR_google_service_creds_json=$GOOGLE_SERVICE_CREDS_JSON && \
-export TF_VAR_jira_api_email=$JIRA_API_EMAIL && \
-export TF_VAR_jira_api_token=$JIRA_API_TOKEN && \
-cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml && \
-cd $GITHUB_WORKSPACE/terraform/terragrunt/$1 && \
+export TF_VAR_repo_root_path=$GITHUB_WORKSPACE
+export TF_VAR_google_service_creds_json=$GOOGLE_SERVICE_CREDS_JSON
+export TF_VAR_jira_api_email=$JIRA_API_EMAIL
+export TF_VAR_jira_api_token=$JIRA_API_TOKEN
+cp $GITHUB_WORKSPACE/config/$1.yml $GITHUB_WORKSPACE/exporter/config.yml
+cd $GITHUB_WORKSPACE/terraform/terragrunt/$1
 terragrunt $2 --terragrunt-source-update --auto-approve=true
